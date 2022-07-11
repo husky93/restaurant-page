dd86803c4b9d226715c9c39fc16cac9278349427
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-11 16:27:43 +0200

    Switch to webpacks development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        ],
+    },
 };

1aea160e7104ec6c54f9b755b10438713b9a3939
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-12 18:00:26 +0200

    Finish about and info sections styling

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -62,14 +62,18 @@ function createMenuSection() {
 
 function createInfoSection() {
     const info = document.createElement('section');
+    const heading = document.createElement('h2');
+    const para = createParagraph('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',['text--info']);
+
     const rowOne = createWrapper(['row']);
     const rowTwo = createWrapper(['row']);
     const rowThree = createWrapper(['row']);
     const rowFour = createWrapper(['row']);
     info.classList.add('wrapper');
     info.classList.add('info');
+    heading.textContent = 'Climatic restaurant - New York';
 
-    rowOne.append(createCard('Elegant', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,\
+    rowOne.append(createCard('Elegance', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,\
     nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,\
     vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'),
     createImage(InfoOne, ['image', 'image--info']));
@@ -78,14 +82,13 @@ function createInfoSection() {
     ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate \
     non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'));
 
-    rowThree.append(createCard('You\'re Invited!', 'Itaque earum rerum hic tenetur a sapiente delectus, \
-    ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'),
-    createImage(InfoThree, ['image', 'image--info']));
+    rowThree.append(createCard('Space outside', 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis \
+    est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est'), createImage(InfoFour, ['image', 'image--info']));
 
-    rowFour.append(createImage(InfoFour, ['image', 'image--info']), createCard('Space outside', 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis \
-    est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est'));
+    rowFour.append(createImage(InfoThree, ['image', 'image--info']), createCard('You\'re Invited!', 'Itaque earum rerum hic tenetur a sapiente delectus, \
+    ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'));
 
-    info.append(rowOne, rowTwo, rowThree, rowFour);
+    info.append(heading, para, rowOne, rowTwo, rowThree, rowFour);
 
     return info;
 }

d672c166348178182d46291c26365c8dfe82a4e3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-13 21:12:53 +0200

    Bugfix - now hero section doesnt overflow

---
+++ dist/src_assets_about_jpg.main.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_assets_about_jpg"],{
+
+/***/ "./src/assets/about.jpg":
+/*!******************************!*\
+  !*** ./src/assets/about.jpg ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "8a6f676f3b55208892b6b56462b69c84.jpg");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX2Fzc2V0c19hYm91dF9qcGcubWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLHlDQUF5QyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9hc3NldHMvYWJvdXQuanBnIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCI4YTZmNjc2ZjNiNTUyMDg4OTJiNmI1NjQ2MmI2OWM4NC5qcGdcIjsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ dist/src_assets_infothree_jpg.main.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_assets_infothree_jpg"],{
+
+/***/ "./src/assets/infothree.jpg":
+/*!**********************************!*\
+  !*** ./src/assets/infothree.jpg ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "9da26c7775eadd7fc7c60b4c44ce9db6.jpg");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX2Fzc2V0c19pbmZvdGhyZWVfanBnLm1haW4uanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFBLGlFQUFlLHFCQUF1Qix5Q0FBeUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvYXNzZXRzL2luZm90aHJlZS5qcGciXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgX193ZWJwYWNrX3B1YmxpY19wYXRoX18gKyBcIjlkYTI2Yzc3NzVlYWRkN2ZjN2M2MGI0YzQ0Y2U5ZGI2LmpwZ1wiOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==

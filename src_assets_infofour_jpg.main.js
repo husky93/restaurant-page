6d21ded3e2fbdccf74c64e20f992a3e3754acfd9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-13 21:12:53 +0200

    Bugfix - now hero section doesnt overflow

---
+++ src_assets_infofour_jpg.main.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_assets_infofour_jpg"],{
+
+/***/ "./src/assets/infofour.jpg":
+/*!*********************************!*\
+  !*** ./src/assets/infofour.jpg ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "d0b12b8a880c68d4981d898e3c13903d.jpg");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX2Fzc2V0c19pbmZvZm91cl9qcGcubWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQUEsaUVBQWUscUJBQXVCLHlDQUF5QyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9hc3NldHMvaW5mb2ZvdXIuanBnIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCJkMGIxMmI4YTg4MGM2OGQ0OTgxZDg5OGUzYzEzOTAzZC5qcGdcIjsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=

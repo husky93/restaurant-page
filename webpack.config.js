2bb140d94f1a38b3a20e87c21ab1d355b489f600
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-28 15:13:16 +0200

    Remove unnecesarry dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
